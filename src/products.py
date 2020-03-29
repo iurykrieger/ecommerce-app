d62db55979fd16a491dcdd82e3037cf3391bd724
Iury Krieger <iurykrieger96@gmail.com>
2020-03-28 22:51:33 -0300

    feat: logic migration to catalog service

--- src/products.py
+++ src/products.py
@@ -1,7 +1,8 @@
-from flask import current_app
+from requests import get, post
 
 def get_products(page=1, per_page=24):
-    return current_app.mongo.db.products.find({}).skip(per_page * page).limit(per_page)
+    products = get("http://catalog:3000/products/list", params={ "page": page, "pageSize": per_page }).json()
+    return products["rows"]
 
 def get_product(product_id):
-    return current_app.mongo.db.products.find_one_or_404({ "id": product_id })
+    return get("http://catalog:3000/products/get", params={ "id": product_id }).json()
